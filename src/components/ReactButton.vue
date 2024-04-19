a85f6bc679cf680ae2d075ecf63afca17f3b3ec0
hjpark625 <zero950@gmail.com>
2024-04-19 11:45:06 +0900

    refactor: ButtonComponent type 지정

--- src/components/ReactButton.vue
+++ src/components/ReactButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="error">error loading button</div>
-  <div v-else ref="root">loading button</div>
+  <div v-else ref="root">loading button...</div>
 </template>
 
 <script lang="ts" setup>
@@ -11,17 +11,14 @@ import { createRoot } from 'react-dom/client'
 
 const props = defineProps<{ text: string }>()
 
-const firstLoad = new Promise((resolve) => setTimeout(resolve, 1000))
-
 async function fetchButton() {
-  await firstLoad
-
-  return (await import('provider/Button')).default
+  const Button = (await import('provider/Button')).default as ({ text }: { text: string }) => JSX.Element
+  return Button
 }
 
-const root = ref(null)
-const error = ref(false)
-const ButtonComponent = ref(null)
+const root = ref<HTMLDivElement | null>(null)
+const error = ref<boolean>(false)
+const ButtonComponent = ref<(({ text }: { text: string }) => JSX.Element) | null>(null)
 
 function updateReactComponent() {
   if (!ButtonComponent.value || !!error.value) return
@@ -43,8 +40,8 @@ fetchButton()
     ButtonComponent.value = button
     updateReactComponent()
   })
-  .catch((err) => {
-    console.log(err)
-    error.value = err
+  .catch((err: unknown) => {
+    error.value = true
+    throw err
   })
 </script>
