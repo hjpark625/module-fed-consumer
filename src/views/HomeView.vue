4ea8418a225d6d2acce3891a3f079b5f6c5e95b0
hjpark625 <zero950@gmail.com>
2024-04-19 10:23:11 +0900

    feat: consumer 완성
    커밋 메시지 재작성

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Welcome to the Home page!</p>
+    <p>{{ message }}</p>
+    <react-button v-if="showButton" :text="message" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ReactButton from '@/components/ReactButton.vue'
+import { ref } from 'vue'
+
+const message = ref('Hello Vue3')
+const showButton = ref(true)
+</script>
